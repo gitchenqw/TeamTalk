f4303799c899f5213463af5613ecbf2a8ba1f7b7
openfree01 <825988516@qq.com>
2019-07-08 10:54:26 +0800

    注释掉密码校验部分代码

--- server/src/db_proxy_server/business/InterLogin.cpp
+++ server/src/db_proxy_server/business/InterLogin.cpp
@@ -47,7 +47,7 @@ bool CInterLoginStrategy::doLogin(const std::string &strName, const std::string 
             char szMd5[33];
             CMd5::MD5_Calculate(strInPass.c_str(), strInPass.length(), szMd5);
             string strOutPass(szMd5);
-            //去掉密码校验
+            // { delete by chenqw 去掉密码校验
             //if(strOutPass == strResult)
             {
                 bRet = true;
